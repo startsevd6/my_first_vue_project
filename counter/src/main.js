276b3c922198549f044b273158e666927196af80
start <startsevd6@gmail.com>
2024-08-15 18:34:06 +0700

    v.0.1.1 added a list of elements that can be deleted

--- counter/src/main.js
+++ counter/src/main.js
@@ -2,7 +2,13 @@ Vue.createApp({
     data() {
         return {
             myHtml: '<h1>Vue 3 App</h1>',
-            title: 'Я есть грунт'
+            title: 'Я есть грунт',
+            person: {
+                firstName: 'Dmitry',
+                lastName: 'Startsev',
+                age: 18,
+            },
+            items: [1, 2, 3, 4, 5, 6]
         }
     }
 }).mount('#app')
