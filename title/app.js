5b083127cdb27c8de4c415e23dcf9db04ac9a8b5
start <startsevd6@gmail.com>
2024-09-11 19:21:05 +0700

    v.0.2.1 new application added

--- title/app.js
+++ title/app.js
@@ -31,3 +31,11 @@ const app = Vue.createApp({
 })
 
 app.mount('#app')
+
+Vue.createApp({
+    data() {
+        return {
+            title: 'Второй заголовок    '
+        }
+    }
+}).mount('#app2')
